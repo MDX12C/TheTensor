c18ac48db838a1733d48a05a4ca2d48ef6827ac4
logman <logman943083678@gmail.com>
2024-03-13 09:24:28 +0800

    Matrix

--- src/Tenser.cpp
+++ src/Tenser.cpp
@@ -1,12 +1,2 @@
-﻿#include"../head/number.hpp"
+﻿#include"../includes/math.hpp"
 namespace Line=Linear_Algebra;
-
-Line::Tenser::Tenser(Line::level alpha,Line::Tenser_type beta){
-    shape=alpha;
-    int lenth=1;
-    for(int i=0;i<shape.Dimensions;i++){
-        lenth*=shape.shape[i];
-    }
-    storage_space=new double[lenth];
-    /**/
-}
