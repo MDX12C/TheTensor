382fada64a8133faf62ecb267a32c83cd8730b12
logman <logman943083678@gmail.com>
2024-03-13 09:07:17 +0800

    init

---
+++ head/number.hpp
@@ -0,0 +1,26 @@
+﻿namespace Linear_Algebra{
+
+    enum Tenser_type{zero,identity};
+
+    struct level{
+        int* shape=nullptr;
+        int Dimensions=0;
+    };
+
+    class Tenser{
+        private:
+            double *storage_space;
+            level shape;
+        public:
+            Tenser(level,Tenser_type);
+            void reset(level,Tenser_type);
+            double *item(level);
+            Tenser T();
+            Tenser inverse();
+            Tenser operator=(Tenser const& tensor);
+            Tenser operator+(Tenser const& tensor);
+            Tenser operator-(Tenser const& tensor);
+            Tenser operator*(Tenser const& tensor);
+    };
+
+};
---
+++ sorce/Tenser.cpp
@@ -0,0 +1,12 @@
+﻿#include"../head/number.hpp"
+namespace Line=Linear_Algebra;
+
+Line::Tenser::Tenser(Line::level alpha,Line::Tenser_type beta){
+    shape=alpha;
+    int lenth=1;
+    for(int i=0;i<shape.Dimensions;i++){
+        lenth*=shape.shape[i];
+    }
+    storage_space=new double[lenth];
+    /**/
+}
