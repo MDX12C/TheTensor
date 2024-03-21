4af424bca693c11e2ac7c1bac5baae0799f96a4a
logman <logman943083678@gmail.com>
2024-03-21 17:00:04 +0800

    Still have a lot of problems

--- includes/math.hpp
+++ includes/math.hpp
@@ -1,45 +1,48 @@
-﻿#include<iostream>
+﻿#include <iostream>
 #ifndef MATRIX
 #define MATRIX
-namespace Linalg{
-    typedef struct{
+namespace Linalg
+{
+    typedef struct
+    {
         int row;
         int lines;
-    }MaShape;
-    bool operator==(MaShape const&,MaShape const&);
+    } MaShape;
+    bool operator==(MaShape const &, MaShape const &);
 
     template <typename Data>
-    class Matrix{
-        private:
-            Data *storage_space;
-            MaShape shape;
-        public:
-            Matrix(MaShape const&,Data* const&);
-            Matrix(MaShape const&,Data const&);
-            ~Matrix();
-            Data *item(MaShape const&);
-            Matrix T();
-            void operator=(Matrix const&);
-            void operator=(Data const&);
-            // void operator+=(Matrix const&);
-            // void operator+=(Data const&);
-            // void operator-=(Matrix const&);
-            // void operator-=(Data const&);
-            // void operator*=(Matrix const&);
-            // void operator*=(Data const&);
-            // void operator/=(Data const&);
-            Matrix operator+(Matrix const&);
-            Matrix operator+(Data const&);
-            Matrix operator-(Matrix const&);
-            Matrix operator-(Data const&);
-            Matrix operator*(Matrix const&);
-            Matrix operator*(Data const&);
-            Matrix operator/(Data const&);
-            void add_line_(Data const&);
+    class Matrix
+    {
+    private:
+        Data *storage_space;
+        MaShape shape;
+        friend void show_Matrix(Matrix<Data> const &);
+    public:
+        Matrix(MaShape const &, Data *const &);
+        Matrix(MaShape const &, Data const &);
+        ~Matrix();
+        Data *item(MaShape const &);
+        Matrix T();
+        void operator=(Matrix const &);
+        void operator=(Data const &);
+        // void operator+=(Matrix const&);
+        // void operator+=(Data const&);
+        // void operator-=(Matrix const&);
+        // void operator-=(Data const&);
+        // void operator*=(Matrix const&);
+        // void operator*=(Data const&);
+        // void operator/=(Data const&);
+        Matrix operator+(Matrix const &);
+        Matrix operator+(Data const &);
+        Matrix operator-(Matrix const &);
+        Matrix operator-(Data const &);
+        Matrix operator*(Matrix const &);
+        Matrix operator*(Data const &);
+        Matrix operator/(Data const &);
+        void add_line_(Data const &);
     };
     template <typename Data>
-    void show_Matrix(Matrix<Data> const&);
+    void show_Matrix(Matrix<Data> const &);
 };
-
 
 #endif
