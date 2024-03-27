e57887049f6bf7049ea1936c71fdf9b8b299768e
logman <logman943083678@gmail.com>
2024-03-27 08:55:13 +0800

    temp

--- test/test_01.cc
+++ test/test_01.cc
@@ -1,14 +1,13 @@
 #include "../includes/math.hpp"
 #include <iostream>
-using namespace std;
 signed main() {
-  float a;
-  Linalg::MaShape ms;
-  cin >> ms.row >> ms.lines >> a;
-  Linalg::Matrix<float> alpha(ms, a);
-  Linalg::show_Matrix(alpha);
-  cin >> ms.row >> ms.lines >> a;
-  alpha.endow_(ms, a);
-  Linalg::show_Matrix(alpha);
-  return 0;
+    float a;
+    Linalg::MaShape ms;
+    std::cin >> ms.row >> ms.lines >> a;
+    Linalg::Matrix<float> alpha(ms, a);
+    Linalg::show_Matrix(alpha);
+    std::cin >> ms.row >> ms.lines >> a;
+    alpha.endow_(ms, a);
+    Linalg::show_Matrix(alpha);
+    return 0;
 }
