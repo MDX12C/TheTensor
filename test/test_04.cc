30f89ca608fdbfd7596e95e41ed6402fd2c04d29
logman <logman943083678@gmail.com>
2024-04-15 20:27:51 +0800

    flag

--- test/test_04.cc
+++ test/test_04.cc
@@ -3,19 +3,21 @@
 #include<cstdlib>
 #include<ctime>
 using namespace std;
+clock_t t1, t2;
 #define show(x) { cout << '\n' << #x << ':' << '\n' << x << '\n'; }
+#define flag(){ t2 = clock();cout<<"t:"<<1000*double(t2-t1)/CLOCKS_PER_SEC<<'\n';t1 = clock() ; }
 namespace bsm = Basic_Math;
 signed main() {
-    srand(time(0));
-    Linalg::MaShape alpha, beta;
-    cin >> alpha.rows >> beta.lines;
-    alpha.lines = beta.rows = Basic_Math::random(1, 15);
-    show(alpha);
-    show(beta);
-    Linalg::Matrix<_Float32> A = bsm::random(alpha, bsm::Float_value * (-1), bsm::Float_value);
-    Linalg::Matrix<_Float32> B = bsm::random(beta, bsm::Float_value * (-1), bsm::Float_value);
-    show(A);
-    show(B);
-    show(Linalg::dot(A, B));
+    t1 = t2 = clock();
+    int alpha;
+    //cin >> alpha;
+    //
+    alpha = 100000;
+    //
+    Linalg::Vector<_Float32> A = bsm::random(alpha, bsm::Float_value * (-1), bsm::Float_value);
+    Linalg::Vector<_Float32> B;
+    flag();
+    B = A;
+    flag();
     return 0;
 }
