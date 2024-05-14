9f0ada03f5b3d50d76623532a40db9deb73f50f9
logman <logman943083678@gmail.com>
2024-05-14 21:40:02 +0800

    v.05.14.00:finish all the bug and let us start the improve

--- includes/basic.hpp
+++ includes/basic.hpp
@@ -26,12 +26,6 @@
 #include<functional>
 #include<xmmintrin.h>
 #endif //_THREAD_MODE_
-// #if defined(__linux__)
-// #include<lwpintrin.h>
-// #include<langinfo.h>
-// #include<lzcntintrin.h>
-// #include<x86_64-linux-gnu>
-// #endif
 #ifdef _SIMD_MODE_
 #include<immintrin.h>
 #include<x86intrin.h>
@@ -88,9 +82,68 @@ namespace Basic_Math {
 	extern std::atomic<bool> set_seed;
 	extern std::atomic<unsigned long long> memory_heap;
 	template <typename Data>
-	int Int_Digits(Data const&);
+	inline int Int_Digits(Data const& alpha) {
+		if constexpr (std::is_same_v<Data, bool>) {
+			return 1;
+		}
+		else {
+			if (alpha < static_cast<Data>(0)) {
+				return (std::floor(std::log10(-alpha)) + 1);
+			}
+			else {
+				return (std::floor(std::log10(alpha)) + 1);
+			}
+		}
+	}
 	template <typename Data>
-	Data random(Data const&, Data const&);
+	inline Data random(Data const& alpha, Data const& beta) {
+		if (!static_cast<bool>(set_seed.load())) {
+			std::srand(int(std::time(0)));
+			set_seed.store(true);
+		}
+		if constexpr (std::is_same_v<Data, int>) {
+#ifdef _DEBUG_MODE_
+			printf("int random\n");
+#endif
+			long long range = static_cast<long long>(beta) - static_cast<long long>(alpha) + 1;
+			long long ans = static_cast<long long>(std::rand()) * 2;
+			if (range < 0) {
+				range *= -1;
+				ans %= range;
+				return static_cast<int>(beta + ans);
+			}
+			else {
+				ans %= range;
+				return static_cast<int>(alpha + ans);
+			}
+		}
+		else if constexpr (std::is_same_v<Data, float>) {
+#ifdef _DEBUG_MODE_
+			printf("float random\n");
+#endif
+			long double range = static_cast<long double>(beta) - static_cast<long double>(alpha) + 1;
+			long double floating = static_cast<long double>(std::rand()) / RAND_MAX;
+			if (range < 0) {
+				range *= -1;
+				floating *= range;
+				return static_cast<float>(beta + floating);
+			}
+			else {
+				floating *= range;
+				return static_cast<float>(alpha + floating);
+			}
+		}
+		else if constexpr (std::is_same_v<Data, bool>) {
+#ifdef _DEBUG_MODE_
+			printf("bool random\n");
+#endif
+			return static_cast<bool>(std::rand() % 2);
+		}
+		else {
+			Data range = beta - alpha + 1;
+			return alpha + (std::rand() % range);
+		}
+	}
 	inline void status() {
 		printf("\n");
 		for (int i = 0; i < terminal_width; i++) printf("-");
