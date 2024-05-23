e8319064525969ba67e5e2d6e21ba304c9720eda
logman <logman943083678@gmail.com>
2024-05-23 17:01:03 +0800

    v.05.23.09:Matrix.hpp finish the declaration of Functions

--- src/Basic.cc
+++ src/Basic.cc
@@ -6,3 +6,37 @@ namespace Basic_Math {
     bool bool_leak = false;
     int int_leak = 0;
 }
+namespace Linalg {
+    /*MaShape operator==
+    Enter: 1.MaShape 2.MaShape
+    compare two MaShape
+    return true if they are equal */
+    bool operator==(MaShape const& alpha, MaShape const& beta)
+    {
+        return (alpha.rows == beta.rows) && (alpha.lines == beta.lines);
+    }
+    /*MaShape operator<<
+    Enter: 1.ostream 2.MaShape
+    show the MaShape's rows and lines
+    return the ostream */
+    std::ostream& operator<<(std::ostream& beta, MaShape const& alpha) {
+        beta << std::noshowpos << "rows: " << alpha.rows << " lines: " << alpha.lines << '\n';
+        return beta;
+    }
+    /*MaShape operator<
+    Enter: 1.MaShape 2.MaShape
+    compare two MaShape
+    return true if the first one is smaller than the second one */
+    bool operator<(MaShape const& alpha, MaShape const& beta)
+    {
+        return (alpha.lines < beta.lines) && (alpha.rows < beta.rows) && (alpha.lines > 0) && (alpha.rows > 0);
+    }
+    /*MaShape operator<=
+    Enter: 1.MaShape 2.MaShape
+    compare two MaShape
+    return true if the first one is smaller than or equal to the second one */
+    bool operator<=(MaShape const& alpha, MaShape const& beta)
+    {
+        return (alpha.lines <= beta.lines) && (alpha.rows <= beta.rows) && (alpha.lines > 0) && (alpha.rows > 0);
+    }
+}
--- test/test_01.cc
+++ test/test_01.cc
@@ -9,13 +9,5 @@ namespace bsm = Basic_Math;
 #define show(x)                                                                \
   { std::cout << #x << ':' << '\n' << x << '\n'; }
 signed main() {
-	Basic_Math::status();
-	const int size = 20;
-	Linalg::Vector<float> alpha = bsm::random(size, static_cast<float>(-1), static_cast<float>(1));
-	show(alpha);
-	bsm::status();
-	alpha.freedom_();
-	show(alpha);
-	bsm::status();
 	return 0;
 }
