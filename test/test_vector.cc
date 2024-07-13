6ccc6d3ee5e6cfe5d486d158172dbaf30dd4adac
banahaker <pn0818x@outlook.com>
2024-07-13 19:17:03 +0800

    feat: Add tests for endow, sum and resize function of Vector

--- test/test_vector.cc
+++ test/test_vector.cc
@@ -18,6 +18,18 @@ int main() {
       10, new int[10]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
   auto c = linalg::dot<int>(*a, *b);
 
-  std::cout << "Dot product: " << c << std::endl;
+  std::cout << "Test: Dot product " << std::endl << c << std::endl;
+
+  std::cout << "Test: sum() for vectors" << std::endl;
+  std::cout << a->sum() << std::endl;
+
+  std::cout << "Test: endow() for vector" << std::endl;
+  a->endow(8, 100);
+  std::cout << *a << std::endl;
+
+  std::cout << "Test: resize() for vector" << std::endl;
+  a->resize(5);
+  std::cout << *a << std::endl;
+
   return 0;
 }
