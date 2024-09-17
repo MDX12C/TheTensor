6375292f66476b5886498551231a0eff3b809db5
logman <logman943083678@gmail.com>
2024-09-17 15:10:31 +0800

    what the fuck

--- temp/test_02/log_main.cc
+++ temp/test_02/log_main.cc
@@ -1,9 +1,11 @@
 #include "log_divide.hpp"
 
 signed main() {
+  log_file::LogSupport::logInit(true);
   LOG_INIT;
   LOG("C:hello world\n");
   int a = 1203;
   LOG("C:a= %d\n", a);
+  log_file::LogSupport::logPack(true);
   return 0;
 }
